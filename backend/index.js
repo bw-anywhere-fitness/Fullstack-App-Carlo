4fdfaaa25814705001576780bc6347342c72b3b1
CarloC24 <carlo.clamucha@gmail.com>
2019-05-18 16:34:52 -0500

    added workout routes

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const server = express();
 const middleware = require("./middleware");
 const session_router = require("./routes/sessions_router");
+const workout_router = require("./routes/workout_router");
 
 //Middleware init
 middleware(server);
 server.use("/session", session_router);
+server.use("/workout", workout_router);
 
 server.listen(9000, () => {
   console.log("server listening on port 9000");
--- backend/routes/workout_router.js
+++ backend/routes/workout_router.js
@@ -1,1 +1,64 @@
 const express = require("express");
+const router = express.Router();
+const workout_helper = require("../workout_helper");
+
+//get all workouts
+router.get("/", async (req, res) => {
+  const response = await workout_helper.getAllWorkouts();
+  if (!response) {
+    return res.json({ message: "There was a error getting all workouts" });
+  }
+  res.json({ data: response });
+});
+
+//get a workout by id
+router.get("/:id", async (req, res) => {
+  const response = await workout_helper.getAWorkoutById(req.params.id);
+  if (!response) {
+    return res.json({ message: "There was a error getting a workouts by id" });
+  }
+  res.json({ data: response });
+});
+
+//get a workout by trainer id
+router.get("/:id", async (req, res) => {
+  const response = await workout_helper.getAWorkoutByTrainerId(req.params.id);
+  if (!response) {
+    return res.json({
+      message: "There was a error getting a workouts by trainer id"
+    });
+  }
+  res.json({ data: response });
+});
+
+//add a workout
+router.post("/", async (req, res) => {
+  const response = await workout_helper.addAWorkout(req.body);
+  if (!response) {
+    return res.json({ message: "There was a error adding a workout" });
+  }
+  res.json({ data: response });
+});
+
+//update a workout
+router.put("/:id", async (req, res) => {
+  const response = await workout_helper.updateAWorkout(req.params.id, req.body);
+  if (!response) {
+    return res.json({ message: "There was a error editing a workout" });
+  }
+  res.json({ message: "Data was successfully updated" });
+});
+
+//delete a workout
+router.delete("/:id", async (req, res) => {
+  const response = await workout_helper.deleteAWorkout(id);
+  if (!response) {
+    return res.json({ message: "There was a error deleting the workout!" });
+  }
+  res.json({
+    message:
+      "Successfully deleted data please do a get request again to get valid data"
+  });
+});
+
+module.exports = router;
